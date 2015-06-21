69123c5076eefe71fdf93ee392fd486a532985e2
GreyAlien502 <carlos.a@del-castillo.net>
2015-06-21 20:39:08 +0000

    packagized

--- sprite.py
+++ sprite.py
@@ -1,4 +1,4 @@
-import manipulate
+import ASCIIImage.manipulate as manipulate
 
 class sprite:
 	def __init__(self,n,state):
@@ -13,3 +13,13 @@ class sprite:
 				self.state[i][1],
 				self.state[i][2])
 		return output
+	def add(name,spacechar):
+		addfile = open(name,'r')
+		addcontents = addfile.read()
+		displayname = name[-name[::-1].index('/'):]
+		if spacechar != '':
+			addcontent = addcontent.translate(''.maketrans(ispacechar,'\u00A0'))
+		self.n.update({displayname:addcontentsi})
+
+	def add(name):
+		add(name,'')
