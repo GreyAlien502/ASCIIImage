e9e64930e38341858bebeaf9a125a3b27e3d1be8
GreyAlien502 <greyalien502@gmail.com>
2015-06-26 18:31:51 -0400

    increased possible screen size

--- play.py
+++ play.py
@@ -12,7 +12,7 @@ frames = int(data[4:10])
 video = data[10:]
 videolecian = video.splitlines()
 for i in range(0,frames):
-	print('\n'*29)
+	print('\n'*99)
 	print('\n'.join(videolecian[i*(length+1):i*(length+1)+length]))
 	sleep(float(videolecian[i*(length+1)+length]))
 	i+=1
