3e98479ec9e8ed4f399210ee45c7608b1a956adf
GreyAlien502 <greyalien502@gmail.com>
2018-02-07 23:14:16 -0500

    changed to version 0.0 because i should never have been 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='asciiimage',
-	version='0.1',
+	version='0.0',
 	description='Work with images made of ASCII text.',
 	url='https://github.com/GreyAlien502/ASCIIImage',
 	author='GreyAlien502',
